908f5d129e7b3997dbdc16f4d35cec4b0ebaca14
Suharevsky <aliksui.ua@mail.ru>
2021-02-06 15:08:59 +0200

    import modules

--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -7,14 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
+import {ForgotPasswordPageModule} from '../forgot-password/forgot-password.module';
+import {LandingPageModule} from '../landing/landing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RegisterPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RegisterPageRoutingModule,
+        ForgotPasswordPageModule,
+        LandingPageModule
+    ],
   declarations: [RegisterPage]
 })
 export class RegisterPageModule {}
